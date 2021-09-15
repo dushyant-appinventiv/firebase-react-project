0d182a30782024492670f5d72bf7a6aa154e3c08
dushyant-appinventiv <dushyant.goyal@appinventiv.com>
2021-09-15 12:48:40 +0530

    initialized react app, created component routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
+import ProjectDetails from "./components/project/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/Signup";
+import CreateProject from "./components/project/CreateProject";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path={"/"} component={Dashboard} />
+        <Route path={"/project/:id"} component={ProjectDetails} />
+        <Route path={"/signin"} component={SignIn} />
+        <Route path={"/signup"} component={SignUp} />
+        <Route path={"/createProject"} component={CreateProject} />
+      </Switch>
+    </Router>
   );
 }
 
