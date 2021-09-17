afa43ddbe3d03a322e82819d88568ce390dd76a2
dushyant-appinventiv <dushyant.goyal@appinventiv.com>
2021-09-17 22:04:03 +0530

    added logout function to logout link

--- src/components/layouts/SignInLinks.js
+++ src/components/layouts/SignInLinks.js
@@ -1,13 +1,16 @@
+import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { signOutUser } from "../../store/actions/authActions";
 
 function SignedIn() {
+  const dispatch = useDispatch();
   return (
     <ul className={"right"}>
       <li>
         <NavLink to={"/createProject"}> {"New Projects"} </NavLink>
       </li>
       <li>
-        <NavLink to={"/"}> {"Log Out"} </NavLink>
+        <a onClick={() => dispatch(signOutUser())}>{"Log Out"}</a>
       </li>
       <li>
         <NavLink to={"/"} className={"btn btn-floating pink ligthen-1"}>
