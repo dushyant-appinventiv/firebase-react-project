4ac25a5f8502e9531cefd9534b6a540068fc4157
dushyant-appinventiv <dushyant.goyal@appinventiv.com>
2021-09-17 22:03:37 +0530

    updated action.types for auth

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,13 +1,33 @@
 const initialState = {
-  user: {
-    id: 1,
-    name: "dummy",
-    email: "dummy@gmail.com",
-  },
+  isLoggedIn: false,
+  msg: null,
 };
 
 const authReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case "LOGIN_FAILURE":
+      console.log("Login Failure");
+      return {
+        ...state,
+        msg: "Auth error , Login failed",
+      };
+    case "LOGIN_SUCCESS":
+      console.log("Login success");
+      return {
+        ...state,
+        isLoggedIn: true,
+        msg: "Login Success",
+      };
+    case "SIGNOUT_SUCCESS":
+      console.log("User signed out success");
+      return {
+        ...state,
+        isLoggedIn: false,
+        msg: "User Signed out!",
+      };
+    default:
+      return state;
+  }
 };
 
 export default authReducer;
